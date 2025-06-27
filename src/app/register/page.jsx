97ffc9a37ae97d3ae9548b4d9c97275862e2b0bb
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-06-27 08:55:43 +0700

    fix self-closing tag syntax in loading spinner

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -160,7 +160,7 @@ const page = () => {
 						className="w-full py-2 px-4 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed hover:cursor-pointer"
 					>
 						{isLoading ? (
-							<span className="loading loading-infinity loading-xl"></span>
+							<span className="loading loading-infinity loading-xl" />
 						) : (
 							"Register"
 						)}
