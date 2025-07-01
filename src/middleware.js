5bd695ee0e1802e0beb99b0e48ba39b07654a8db
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-07-01 10:09:07 +0700

    add route protection and redirection logic
    Implement protected and public route handling in middleware to:
    - Redirect unauthenticated users from protected routes to login
    - Redirect authenticated users from public routes to dashboard

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,24 @@
 import { NextResponse } from "next/server";
 import getAuthUser from "./lib/getAuthUser";
 
-export async function middleware(request) {
-	const user = await getAuthUser();
-	console.log(user);
+const protectedRoutes = ["/dashboard", "/"];
+const publicRoutes = ["/login", "/register"];
+
+export async function middleware(req) {
+	const path = req.nextUrl.pathname;
+	const isProtected = protectedRoutes.includes(path);
+	const isPublic = publicRoutes.includes(path);
+
+	const authUser = await getAuthUser();
+	const userID = authUser?.id;
+
+	if (isProtected && !userID) {
+		return NextResponse.redirect(new URL("/login", req.nextUrl));
+	}
+
+	if (isPublic && userID) {
+		return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+	}
+
+	return NextResponse.next();
 }
